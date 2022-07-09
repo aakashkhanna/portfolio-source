f4606c9d8b224d4e09ca6dc3d7dd26d1db9fc4d8
Aakash Khannam <aakashkh13@gmail.com>
2022-07-09 18:03:54 +0530

    Added new text for front page

--- src/portfolio.js
+++ src/portfolio.js
@@ -340,9 +340,9 @@ const publicationsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "animated_ashutosh.png",
+    profile_image_path: "",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "You can message me, I will reply within 24 hours. I can help you with Docker, .Net Core 3.1, FastAPI, Python, Cloud and Opensource Development.",
   },
   addressSection: {
     title: "City",
